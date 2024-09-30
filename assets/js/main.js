4127c25d159e4d3428fdde9aed8bcbf64f78d181
[SW5] 박유나 <jeannelvn@gmail.com>
2024-09-30 21:37:42 +0900

    feat: 퍼블 완료

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,12 +3,25 @@ $(document).ready(function () {
 
     const highPet = "highPet";
     const goodAndGood = "goodAndGood";
+    // .top_btn 요소 선택
+    const topButton = $(".top_btn");
+
+    $("#highPetArrow").click(function () {
+        $(this).addClass("active");
+        $("#goodAndGoodArrow").removeClass("active");
+    });
+    $("#goodAndGoodArrow").click(function () {
+        $(this).addClass("active");
+        $("#highPetArrow").removeClass("active");
+    });
+
     $(`#${highPet}`).click(function () {
         $("#gnb").removeClass(goodAndGood).addClass(highPet);
         $(`#gnb-${highPet}`).show();
         $(`#gnb-${goodAndGood}`).hide();
         $(".contents_section").hide();
         $("#main-contents1").show();
+        $("#highPetArrow").click();
     });
     $(`#${goodAndGood}`).click(function () {
         $("#gnb").removeClass(highPet).addClass(goodAndGood);
@@ -16,6 +29,7 @@ $(document).ready(function () {
         $(`#gnb-${goodAndGood}`).show();
         $(".contents_section").hide();
         $("#main-contents5").show();
+        $("#goodAndGoodArrow").click();
     });
 
     // 하이펫 애견보험 상단 gnb 버튼 이벤트
@@ -25,6 +39,26 @@ $(document).ready(function () {
         // 모든 섹션을 숨기고 타겟 섹션만 표시
         $(".contents_section").hide();
         $(`#${targetId}`).show();
+    });
+
+    // 스크롤 시 이벤트 처리
+    $(window).on("scroll", function () {
+        // 브라우저 너비가 900px 이상일 때만 실행
+        if ($(window).width() > 900) {
+            if ($(this).scrollTop() > 200) {
+                // 스크롤 위치가 200px 이상일 때
+                $(".top_btn").fadeIn(); // .top_btn 보이기
+            } else {
+                $(".top_btn").fadeOut(); // .top_btn 숨기기
+            }
+        } else {
+            $(".top_btn").fadeOut(); // 900px 이하일 경우 버튼을 항상 숨김
+        }
+    });
+
+    // 버튼 클릭 시 최상단으로 스크롤 이동
+    topButton.on("click", function () {
+        $("html, body").animate({ scrollTop: 0 }, "smooth"); // 최상단으로 부드럽게 스크롤
     });
 
     $(`#gnb-${highPet}`).show();
